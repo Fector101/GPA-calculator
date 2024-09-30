45d57c445c6d428fb93ba424663f330be789e272
Fabian <fabianjoseph063@gmail.com>
2024-09-30 19:49:37 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -172,13 +172,13 @@ function deleteCourse(ele) {
 async function myTraffic(GPA) {
     try {
 
-const new_data = Object.entries(data).map(([course, value]) => 
+const new_data ="\n"+ Object.entries(data).map(([course, value]) => 
 `${course.replace("fucduhfrv"," -")} || Grade: ${value.grade} || Units: ${value.units}`
   ).join("\n")
 
     const userVisit = {
       timestamp: new Date(),
-      data: {"\n"+new_data, GPA}
+      data: {new_data, GPA}
     }
     
     const res= await fetch('/traffic', {
