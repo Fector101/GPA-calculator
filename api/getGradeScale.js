dca15fedd7267d2df81de23141c0c79d1fa8e50f
Fabian <fabianjoseph063@gmail.com>
2024-10-11 00:19:55 +0100

    Delete api/getGradeScale.js

--- api/getGradeScale.js
+++
@@ -1,6 +0,0 @@
-import { get } from '@vercel/edge-config';
-
-export default async function handler(req, res) {
-  const studentGrades = await get('greeting');  // Use the key you just added
-  res.status(200).json({ grades: studentGrades });
-}
