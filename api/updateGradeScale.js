62c2d4a8fe286cec1cd7192aac44975a35029b42
Fabian <fabianjoseph063@gmail.com>
2024-10-11 00:20:09 +0100

    Delete api/updateGradeScale.js
    Next js

--- api/updateGradeScale.js
+++
@@ -1,8 +0,0 @@
-import { update } from '@vercel/edge-config';
-
-export default async function handler(req, res) {
-  await update({
-    greeting: { "A": 90, "B": 80, "C": 70 }  // Update or change the value
-  });
-  res.status(200).json({ message: 'Grades updated' });
-}
